936427e31543dd23b2a54bf663a6025edfae3f49
amankapur007 <akapur07@gmail.com>
2022-08-26 17:27:51 +0530

    1. Props

---
+++ components/Greet.tsx
@@ -0,0 +1,12 @@
+type GreetProps = {
+  name: String;
+};
+import React = require('react');
+
+export const Greet = (props: GreetProps) => {
+  return (
+    <div>
+      <h2>Welcome {props.name} , you have 10 unread messages</h2>
+    </div>
+  );
+};
